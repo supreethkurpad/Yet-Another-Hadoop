a06cbe23edfa2de6387c700241657593567dcd23
swarupa29 <swarupav01@gmail.com>
2021-11-16 14:49:24 +0530

    unload dfs

--- unload_dfs.py
+++ unload_dfs.py
@@ -7,5 +7,8 @@ tmp = open(os.path.join(HADOOP_HOME, 'tmp', 'pids.txt'), 'r')
 for child in tmp.read().split('\n'):
     if child is None or child == "": break
     print("Killing process", child)
-    os.kill(int(child), SIGTERM)
+    try:
+        os.kill(int(child), SIGTERM)
+    except:
+        print("process not found")
 tmp.close()
