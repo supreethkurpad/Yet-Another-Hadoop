4fd8ac673f0a0617650144c829ff88ef756a2d5a
Suvigya Jain <suvigyajain1@gmail.com>
2021-12-05 06:40:45 +0530

    updated installer

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ source $MYHADOOP_HOME/hadoop_env/bin/activate
 pip install -r $MYHADOOP_HOME/requirements.txt
 cd $MYHADOOP_HOME
 mkdir directories
+mkdir directories/namenodes
+mkdir directories/datanodes
 mkdir tmp
 mkdir configs
 touch configs/config.json configs/dfs_setup_config.json
